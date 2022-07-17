c6204a99d287460f976d752d7d24d6f50f410b4b
Brendan Cordova <cordovalegacy19@gmail.com>
2022-07-17 14:05:56 -0700

    coin flip counting heads, using promises in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+  }
+
+  function fiveHeads() {
+        return new Promise( (resolve, reject) => {
+            let headsCount = 0;
+            let attempts = 0;
+            let maxAttempts = 100;
+
+            while(headsCount < 5 && attempts <= maxAttempts) {
+              attempts++;
+              let result = tossCoin();
+              console.log(`${result} was flipped`);
+              if (result === "heads") {
+                headsCount++
+              } else {
+                headsCount = 0;
+              }
+            }
+            if( attempts <= maxAttempts) {
+              resolve (`It took ${attempts} tries to flip five "heads"`);
+            } else {
+              reject(`Attempts have exceeded ${maxAttempts} flips.`);
+            }
+        });
+    }
+    fiveHeads()
+        .then( res => console.log(res) )
+        .catch( err => console.log(err) );
+    console.log( "When does this run now?" );
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={App}>Flip Coin</button>
+      <h2>Check the console for your results!</h2>
     </div>
   );
 }
